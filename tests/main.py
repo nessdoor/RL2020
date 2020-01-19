1d0e4ca4523e09637989230580ccad2bdd73c113
Tomas Antonio Lopez <tomasantonio.lopez@mail.polimi.it>
2020-01-19 15:09:20 +0100

    Added fuzz-test generator script

---
+++ tests/main.py
@@ -0,0 +1,87 @@
+from random import seed, randrange
+from typing import Collection, Sequence, List
+
+
+def generate_test_zones(quantity: int) -> List[int]:
+    zones = list()
+    covered_addresses = set()
+    while len(zones) < quantity:
+        candidate = randrange(0, 128)
+
+        if candidate not in covered_addresses:
+            zones.append(candidate)
+            
+            for a in range(candidate, candidate + 4):
+                covered_addresses.add(a)
+
+    return zones
+
+
+def generate_test_inputs(zones: Collection[int]) -> List[int]:
+    input_set = set()
+
+    input_set.add(0)
+    input_set.add(1)
+    
+    for wz in zones:
+        for address in range(wz - 2, wz + 2):
+            if 0 <= address <= 127:
+                input_set.add(address)
+
+    input_set.add(126)
+    input_set.add(127)
+
+    return list(input_set)
+
+
+def encode_address(zones: Sequence[int], address: int) -> int:
+    output = address
+    
+    for wz in zones:
+        if wz <= address < wz + 4:
+            return 128 | (zones.index(wz) << 4) | (1 << (address - wz))
+
+    return output
+
+
+def generate_wz(index: int, base: int) -> str:
+    return " 100 ns 0 0 1 " + '{:04X}'.format(index) + " " + '{:02X}'.format(base)
+
+
+def generate_input(address: int) -> str:
+    return " 100 ns 0 0 1 0008 " + '{:02X}'.format(address)
+
+
+def generate_start() -> str:
+    return " 100 ns 0 1 0 0000 00"
+
+
+def generate_checkpoint(expected: int) -> str:
+    return "V " + '{:02X}'.format(expected)
+
+
+def generate_reset() -> str:
+    return " 100 ns 1 0 0 0000 00"
+
+
+def generate_pause() -> str:
+    return " 100 ns 0 0 0 0000 00"
+
+
+if __name__ == '__main__':
+    seed()
+    wzs = generate_test_zones(8)
+    inputs = generate_test_inputs(wzs)
+
+    for z in wzs:
+        print(generate_wz(wzs.index(z), z))
+    
+    print(generate_input(inputs[-1]))
+    print(generate_reset())
+    print(generate_start())
+    print(generate_checkpoint(encode_address(wzs, inputs.pop())))
+
+    for i in inputs:
+        print(generate_input(i))
+        print(generate_start())
+        print(generate_checkpoint(encode_address(wzs, i)))
